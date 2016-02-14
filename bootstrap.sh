222480ccebcbdfbb4e87d4950a0668918a1abe7c
brenj <brenj@mailbox.org>
2016-02-13 19:28:32 -0600

    Create symbolic link to bash_profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,6 +27,7 @@ git clone https://github.com/brenj/dotfiles.git
 echo "[+] Linking configuration files"
 ln -s dotfiles/.bashrc .bashrc
 ln -s dotfiles/.bash_aliases .bash_aliases
+ln -s dotfiles/.bash_profile .bash_profile
 ln -s dotfiles/.gvimrc .gvimrc
 ln -s dotfiles/.tmux.conf .tmux.conf
 ln -s dotfiles/.vim .vim
