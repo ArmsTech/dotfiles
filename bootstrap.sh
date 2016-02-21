6910a6ffcce1f51ad921a3aa7e4613b6dbd99558
brenj <brenj@mailbox.org>
2016-02-21 17:11:42 -0600

    Remove home directory references

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,32 +8,32 @@ echo "[+] Cloning dotfiles"
 git clone https://github.com/brenj/dotfiles.git
 
 echo "[+] Linking configuration files"
-ln -s ~/dotfiles/.bashrc .bashrc
-ln -s ~/dotfiles/.bash_aliases .bash_aliases
-ln -s ~/dotfiles/.bash_profile .bash_profile
-ln -s ~/dotfiles/.gvimrc .gvimrc
-ln -s ~/dotfiles/.tmux.conf .tmux.conf
-ln -s ~/dotfiles/.vim .vim
-ln -s ~/dotfiles/.vimrc .vimrc
-ln -s ~/dotfiles/.xmonad .xmonad
+ln -s dotfiles/.bashrc .bashrc
+ln -s dotfiles/.bash_aliases .bash_aliases
+ln -s dotfiles/.bash_profile .bash_profile
+ln -s dotfiles/.gvimrc .gvimrc
+ln -s dotfiles/.tmux.conf .tmux.conf
+ln -s dotfiles/.vim .vim
+ln -s dotfiles/.vimrc .vimrc
+ln -s dotfiles/.xmonad .xmonad
 
 # Get script for git prompt
 wget -O .git-prompt.sh -q "${RAW_URL}"/git/git/master/contrib/completion/git-prompt.sh
 
 echo "[+] Installing node"
 wget -q -O - ${RAW_URL}/creationix/nvm/v0.30.2/install.sh |bash
-. ~/.nvm/nvm.sh
-nvm install node &>/dev/null && nvm current >~/.nvmrc
+. .nvm/nvm.sh
+nvm install node &>/dev/null && nvm current >.nvmrc
 
 echo "[+] Configuring vim"
-mkdir ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir .vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 vim +PluginInstall +qall &>/dev/null
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --tern-completer &>/dev/null
+cd .vim/bundle/YouCompleteMe
+install.py --clang-completer --tern-completer &>/dev/null
 cd
 
 echo "[+] Configuring NeoVim"
-cd ~/.config
-ln -s ~/dotfiles/.vim nvim
+cd .config
+ln -s dotfiles/.vim nvim
 cd
